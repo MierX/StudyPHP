e7df7ae44c45022486a1403cc0891eacca2f69a9
MierX <1214915547@qq.com>
2021-04-16 18:07:55 +0800

    29

---
+++ 2015/advanced/04/01/Model/GoodsModel.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * author：MierX
+ * github：https://github.com/MierX/StudyPhp
+ * create：21.4.13 11:25
+ * motto ：有志者，事竟成
+ */
+
+namespace Model;
+
+use Think\Model;
+
+class GoodsModel extends Model
+{
+
+}
